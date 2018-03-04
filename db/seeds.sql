4c77cb18912ff5088fc349d5d079ab40b7e7e8c9
Maxwell Cutri <mpcutri@gmail.com>
2018-03-04 13:00:14 -0700

    Initial Files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon & Bleu Cheese Burger');
+INSERT INTO tasks (task) VALUES ('Ahi Tuna Burger');
+INSERT INTO tasks (task) VALUES ('Classic Double Cheese Burger');
+
+SELECT * FROM burgers;
